93d81cc4a2d7332e4d40826548bd4e15ad151925
Aniruddh-ctrl <aniruddh7390922053@gmail.com>
2025-11-20 21:14:49 +0530

    Update TriangleValidator.java

--- TriangleValidator.java
+++ TriangleValidator.java
@@ -1,20 +1,20 @@
 
-// import java.util.Scanner;
+ import java.util.Scanner;
 
 
-// public class TriangleValidator{
-//     public static void main(String[] args) {
-//         Scanner scanner=new Scanner(System.in);
-//         System.out.println("Enter sides of triangle");
-//         System.out.println("First Side :");
-//         int a = scanner.nextInt();
-//         System.out.println("Second Side :");
-//         int b = scanner.nextInt();
-//         System.out.println("Third Side :");
-//         int c = scanner.nextInt();
-//         if(a+b>c && b+c>a && c+a>b)
-//         System.out.println("Valid");
-//         else
-//         System.out.println("InValid");  
-//     }
-// }
+ public class TriangleValidator{
+     public static void main(String[] args) {
+         Scanner scanner=new Scanner(System.in);
+         System.out.println("Enter sides of triangle");
+         System.out.println("First Side :");
+         int a = scanner.nextInt();
+         System.out.println("Second Side :");
+         int b = scanner.nextInt();
+         System.out.println("Third Side :");
+         int c = scanner.nextInt();
+         if(a+b>c && b+c>a && c+a>b)
+         System.out.println("Valid");
+         else
+         System.out.println("InValid");  
+     }
+ }
